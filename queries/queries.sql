849f58d8e0e316ca663222a07b5ffcfc4dd94f2c
guzeloglusoner <guzeloglusoner@gmail.com>
2021-04-03 16:57:24 +0300

    KSQL modification performed.

--- queries/queries.sql
+++ queries/queries.sql
@@ -1,9 +1,11 @@
 CREATE STREAM EVENTS WITH (
     kafka_topic = 'dbserver1.public.MY_TABLE',
-    value_format = 'avro',
-    timestamp='after -> timestamp',
-    timestamp_format='yyyy-MM-dd''T''HH:mm:ss.SSSz'
+    value_format = 'avro'
 );
+
+CREATE STREAM EVENTS_EXPLODED AS
+    SELECT EXTRACTJSONFIELD(after -> data, '$.event') AS EVENT_NAME, after
+    FROM EVENTS;
 
 -- ORDERS TO BE FILTERED.
 CREATE STREAM ORDERS AS
@@ -18,8 +20,8 @@ CREATE STREAM ORDERS AS
     EXTRACTJSONFIELD(after -> data, '$.review_value_speed') AS REVIEW_VALUE_SPEED,
     EXTRACTJSONFIELD(after -> data, '$.context_traits_persona') AS CONTEXT_TRAITS_PERSONA,
     EXTRACTJSONFIELD(after -> data, '$.orderStationManufacterer') AS ORDER_STATION_MANUFACTURER
-    FROM EVENTS
-    WHERE after -> data '$.event' in ('order/execute/customer/status/processing','order/execute/customer/status/payment','order/execute/customer/status/printing','order/execute/customer/status/successful');
+    FROM EVENTS_EXPLODED
+    WHERE EVENT_NAME =  'order/execute/customer/status/processing' OR EVENT_NAME = 'order/execute/customer/status/payment' OR EVENT_NAME = 'order/execute/customer/status/printing' OR EVENT_NAME = 'order/execute/customer/status/successful';
 
 
 
@@ -28,6 +30,6 @@ CREATE STREAM ORDERS AS
      SELECT supplier_id,
          SUM(CASE WHEN EVENT_NAME = 'order/execute/customer/status/processing' THEN 1 ELSE 0 END),
          SUM(CASE WHEN EVENT_NAME in ('order/execute/customer/status/processing','order/execute/customer/status/printing') THEN 1 ELSE 0 END) AS ACCEPTANCE_RATE
-     FROM ORDERS_NEW
+     FROM ORDERS
      GROUP BY supplier_id
      EMIT CHANGES;
