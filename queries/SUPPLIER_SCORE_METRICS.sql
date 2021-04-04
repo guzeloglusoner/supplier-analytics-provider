3071cf4f18ad96a56b06aecf45d96fcac8539fb7
unknown <guzeloglusoner@gmail.com>
2021-04-04 15:37:15 +0300

    Typo fixed.

--- queries/SUPPLIER_SCORE_METRICS.sql
+++ queries/SUPPLIER_SCORE_METRICS.sql
@@ -6,9 +6,9 @@ SELECT f.supplier_id,
     AVG((CAST(NULLIF(f.REVIEW_VALUE_SPEED,'null')  AS FLOAT64) + CAST(NULLIF(f.REVIEW_VALUE_PRINT_QUALITY,'null')  AS FLOAT64)) / 2) as value
  FROM (
      SELECT *
-      FROM  `venue-organization.supplier_analytics.REVIEW_EVENTS` r1
-      WHERE r1.order_id NOT IN (SELECT r2.order_id FROM `venue-organization.supplier_analytics.REVIEW_EVENTS` r2 WHERE r2.event_name = "DELETED") -- Exclude deleted reviews
-      and r1.timestamp = (SELECT MAX(timestamp) FROM `venue-organization.supplier_analytics.REVIEW_EVENTS` r3 WHERE r1.order_id = r3.order_id) -- Get the latest review for each review sequence
+      FROM  `supplier_analytics.REVIEW_EVENTS` r1
+      WHERE r1.order_id NOT IN (SELECT r2.order_id FROM `supplier_analytics.REVIEW_EVENTS` r2 WHERE r2.event_name = "DELETED") -- Exclude deleted reviews
+      and r1.timestamp = (SELECT MAX(timestamp) FROM `supplier_analytics.REVIEW_EVENTS` r3 WHERE r1.order_id = r3.order_id) -- Get the latest review for each review sequence
  ) f
  GROUP BY f.supplier_id, EXTRACT(DATE FROM TIMESTAMP(f.timestamp))
  
@@ -20,6 +20,6 @@ SELECT f.supplier_id,
     --SUM(CASE WHEN EVENT_NAME in ('order/execute/customer/status/processing','order/execute/customer/status/printing') THEN 1 ELSE 0 END) AS TOTAL_ORDERS,
     "acceptance_ratio" as metric,
     100 * (SUM(CASE WHEN EVENT_NAME = 'order/execute/customer/status/printing' THEN 1 ELSE 0 END) / SUM(CASE WHEN EVENT_NAME in ('order/execute/customer/status/processing','order/execute/customer/status/printing') THEN 1 ELSE 0 END))  AS value,
-FROM `venue-organization.supplier_analytics.ORDER_EVENTS`
+FROM `supplier_analytics.ORDER_EVENTS`
 WHERE EVENT_NAME in ('order/execute/customer/status/printing','order/execute/customer/status/processing')
 GROUP BY supplier_id, EXTRACT(DATE FROM TIMESTAMP(timestamp));
